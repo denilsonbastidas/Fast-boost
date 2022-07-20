ebfe9972a61bbcdd2bd1d3fb25e6447bd8dfff38
Denilson <denilson@socialgest.net>
2022-07-20 13:55:19 -0400

    added gitignore

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Instagram = require('instagram-web-api')
 const App = express()
+
 // const username = 'incodeweb'
 // const password = 'danielgustavodeni'
 
