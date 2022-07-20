659d7ea0f9df1cb04874d417322d95e7f4cb82f8
Denilson <denilson@socialgest.net>
2022-07-20 10:42:30 -0400

    initial project.

--- src/app.js
+++ src/app.js
@@ -4,15 +4,4 @@ const App = express()
 // const username = 'incodeweb'
 // const password = 'danielgustavodeni'
 
-
-const { username, password } = process.env
-
-const client = new Instagram({ username: 'incodeweb', password: 'danielgustavodeni' }, { language: 'es-CL' });
-
-(async () => {
-    await client.login()
-    const profile = await client.getProfile()
-
-    console.log(profile)
-})()
 module.exports = App
